c80f7221a9e9324d6e2238c1a2e70ab7910a14e3
Artemy <124582137+ArtemijsRadionovs@users.noreply.github.com>
2024-11-10 20:08:26 +0200

    Added React project

---
+++ ocr-vite-git/src/App-test.jsx
@@ -0,0 +1,70 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <div style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(0deg, white 0%, white 100%)', border: '1px black solid', backgroundImage: 'url("/images/background.jpg")'}}>
+        <div style={{width: '100%', height: '100%', padding: 12, background: '#B3261E', borderRadius: 8, overflow: 'hidden', border: '1px #C00F0C solid', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+          <div style={{width: 579, height: 53, color: '#FEE9E7', fontSize: 64, fontFamily: 'Inter', fontWeight: '400', lineHeight: 64, wordWrap: 'break-word'}}>Create CO2 Report</div>
+        </div>
+        <div style={{left: 358, top: 627, position: 'absolute', textAlign: 'center', color: '#646D6E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', lineHeight: 28, wordWrap: 'break-word'}}>Press the button to find out what is your CO2 emission</div>
+        <div style={{width: 62, height: 27, left: 1290, top: 943, position: 'absolute', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>© RTU</div>
+        <div style={{height: 58, left: 53, top: 900, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex'}}>
+            <div style={{alignSelf: 'stretch', justifyContent: 'center', alignItems: 'flex-start', display: 'inline-flex'}}>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Ilja Fonarevs</div>
+                </div>
+                <div style={{flex: '1 1 0', height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{textAlign: 'center'}}></div>
+                </div>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Artemijs Radionovs</div>
+                </div>
+                <div style={{width: 191, height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{textAlign: 'center'}}></div>
+                </div>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Aļģerts Kligulis</div>
+                </div>
+                <div style={{width: 144, height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{textAlign: 'center'}}></div>
+                </div>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Alisa Mamikova</div>
+                </div>
+                <div style={{width: 142, height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{textAlign: 'center'}}></div>
+                </div>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Alina Eksta</div>
+                </div>
+                <div style={{width: 143, height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{textAlign: 'center'}}></div>
+                </div>
+                <div style={{height: 30, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', borderBottom: '1px #B2B2B2 solid', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center', color: '#767676', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>Daniels Tehanovičs</div>
+                </div>
+                <div style={{width: 170, height: 20, paddingTop: 4, paddingBottom: 4, borderTopLeftRadius: 4, borderTopRightRadius: 4, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <div style={{flex: '1 1 0', textAlign: 'center'}}></div>
+                </div>
+            </div>
+        </div>
+        <div style={{paddingLeft: 12, paddingRight: 12, paddingTop: 4, paddingBottom: 4, left: 1278, top: 900, position: 'absolute', borderTopLeftRadius: 4, borderTopRightRadius: 4, borderBottom: '1px #757575 solid', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{color: '#303030', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 22.40, wordWrap: 'break-word'}}>3.grupa</div>
+        </div>
+        <div style={{width: 176, height: 70, padding: 12, left: 979, top: 63, position: 'absolute', background: '#818181', borderRadius: 8, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+            <div style={{color: '#F5F5F5', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', lineHeight: 24, wordWrap: 'break-word'}}>Sign up</div>
+        </div>
+        <div style={{width: 176, height: 70, padding: 12, left: 1176, top: 62, position: 'absolute', background: '#818181', borderRadius: 8, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+            <div style={{color: '#F5F5F5', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', lineHeight: 24, wordWrap: 'break-word'}}>Log in</div>
+        </div>
+        <img style={{width: 230, height: 230, left: 66, top: 45, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.50) inset', borderRadius: 279}} src="./images/logo.png" alt='logo' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
